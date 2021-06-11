a3d6a0458924a33dd35e6793fc2037e361792f0b
crclz <crucialize@foxmail.com>
2021-06-11 19:34:28 +0800

    Load json data

--- CVUI/src/app/app.component.ts
+++ CVUI/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AnimeRepositoryService } from './ddd/repository/anime-repository.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CVUI';
+
+  /**
+   *
+   */
+  constructor(public animeRepository: AnimeRepositoryService) {
+    console.log("HELLOOOOOOO")
+  }
 }
--- CVUI/src/app/ddd/repository/anime-repository.service.ts
+++ CVUI/src/app/ddd/repository/anime-repository.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import rawAnime from '../../../../../data/3-animes.json'
+import { Anime } from '../domain/anime';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimeRepositoryService {
 
-  constructor() { }
+  public animes: Anime[] = []
+
+  constructor() {
+    // console.log(rawAnime)
+    for (var x of rawAnime) {
+      var anime = new Anime();
+      anime.title = x.title;
+      anime.watchUrl = x.viewUrl;
+
+      for (var [character, cv] of x.cv) {
+        anime.cvChar.set(cv, character);
+      }
+
+      this.animes.push(anime)
+    }
+
+    console.log(this.animes)
+  }
 }
